5a60bf29d33fbe448d9435999f3b7203e9a936be
aleksireede <79536980+aleksireede@users.noreply.github.com>
2022-02-05 20:04:51 +0200

    Create Main.py
    Version 1 initial commit

---
+++ Main.py
@@ -0,0 +1,25 @@
+import os, fnmatch
+
+
+def main():
+    for (root,dirs,files) in os.walk(os.getcwd(), topdown=True):
+        for file in files:
+            if '.mp4' not in file:
+                continue
+            if 'redditsave.com_' in file:
+                string_list = file.split('_')
+                new_string = ''
+                for X in range(len(string_list)):
+                    print(string_list[X])
+                    if string_list[X] == 'redditsave.com':
+                        continue
+                    if X == 1:
+                        new_string=new_string+string_list[X].capitalize()+' '
+                        continue
+                    new_string=new_string+string_list[X]+' '
+                new_filename = new_string.split('-')
+                print(new_filename)
+            os.rename(file,new_filename[0]+'.mp4')
+
+
+if __name__ == "__main__":main()
